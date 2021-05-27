05a91572132ea34d814615436be567e836e773d1
AmbrishaVaibhav <83808541+AmbrishaVaibhav@users.noreply.github.com>
2021-05-28 01:43:38 +0530

    Delete voice record.py

--- voice record.py
+++
@@ -1,9 +0,0 @@
-import sounddevice as sd
-import wavio as wv
-
-frequency= 44100
-duration = 10
-recording = sd.rec(int(duration * frequency), samplerate=frequency, channels=2)
-
-sd.wait()
-wv.write("recording1.wav", recording, frequency, sampwidth=2)
